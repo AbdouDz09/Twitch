f146d80ac8532e29caebcf337bed32d7ac6b0950
Abdou2004 <45575757+AbdouDz09@users.noreply.github.com>
2019-06-24 11:07:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,39 +2,27 @@ const Discord = require('discord.js')//Toxic Codes // n3k4a is one
 const client = new Discord.Client();//Toxic Codes // n3k4a is one
 client.on('ready', function(){//Toxic Codes // n3k4a is one
  console.log(`Logged in as ${client.user.tag}!`); //Toxic Codes // n3k4a is one
-   
-  var adminprefix = "*";
-const devs = ['461468630773661699','466734861558743041'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-	
-     if (message.content.startsWith(adminprefix + 'setg')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
-  } else
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
-  } else
-     
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'setavatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else
-	if (message.content.startsWith(adminprefix + 'sett')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/abdoufersaoui_dz");
-    message.channel.sendMessage(`تم تغيير تويتش البوت إلى  ${argresult}`)
-	}
+    
+   var large = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293']; // صور الكبيرة
+ var small = ['483055660209012736','480169573530861578','483055655800930315'];  // صور الصغيرة
+    setInterval(function (){  
+    client.user.setPresence({
+  status: 'dnd', 
+ game: { 
+    type: 1,
+    name: ' Dream !! ',
+     url: 'https://www.twitch.tv/abdoufersaoui_dz',
+     state: `new stream code `,
+    application_id: '461468630773661699',  
+     assets: {
+        small_text: ' Abdou 100% Dz ' ,
+        large_text: `Dream Server !! :)` },
+    large_image:  `${large[Math.floor(Math.random() * large.length)]}`,
+    small_image:   `${small[Math.floor(Math.random() * small.length)]}`,
+    }
+  
+    });
+    }, 5000);
 });
 	
 	
