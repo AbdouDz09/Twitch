7a7277f862adcd93b304f44376c693540810f67f
Abdou2004 <45575757+AbdouDz09@users.noreply.github.com>
2019-06-24 10:57:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,12 @@ const client = new Discord.Client();//Toxic Codes // n3k4a is one
 client.on('ready', function(){//Toxic Codes // n3k4a is one
  console.log(`Logged in as ${client.user.tag}!`); //Toxic Codes // n3k4a is one
    
-   // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293']; // صور اضافيه
-   var s = ['483055660209012736','480169573530861578','483055655800930315'];  // صور  الي بتشغل
+   var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
     setInterval(function (){//Toxic Codes // n3k4a is one  
     client.user.setPresence({//Toxic Codes // n3k4a is one
  game: { //Toxic Codes // n3k4a is one
     type: 1,//Toxic Codes // n3k4a is one
-     url: 'https://www.twitch.tv/n3k4a',//Toxic Codes // n3k4a is one
+     url: 'https://www.twitch.tv/abdoufersaoui_dz',//Toxic Codes // n3k4a is one
     name: 'im try banned ',  // الكلام الي في التوتش//Toxic Codes // n3k4a is one
     application_id: '461468630773661699', // ايدي البوت او ايدي الحساب حقك //Toxic Codes // n3k4a is one
      assets: {//Toxic Codes // n3k4a is one
